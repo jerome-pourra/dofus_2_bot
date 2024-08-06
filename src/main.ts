f550daac733717d5db20c9f433f3e75161cfea8b
Jérôme Pourra <j.pourra.perso@gmail.com>
2024-08-06 20:19:20 +0200

    move imports to match with new path

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
-import * as zlib from 'zlib';
 import { Mitm } from "./network/Mitm";
 import { config } from "./config";
 import { FridaHookManager } from "./frida/hook";
-import { PakProtocol2 } from "./network/com/ankamagames/jerakine/resources/protocols/impl/PakProtocol2";
-import { Map } from "./network/com/ankamagames/atouin/data/map/Map";
-import { CustomBuffer } from './network/utils/CustomBuffer';
+import { PakProtocol2 } from './com/ankamagames/jerakine/resources/protocols/impl/PakProtocol2';
 
 let maps = PakProtocol2.getFilesIndex("C:/Users/jerom/AppData/Local/Ankama/Dofus/content/maps/maps0.d2p");
 // console.log(maps);
 
+// import * as zlib from 'zlib';
 // if (maps.has(mapId)) {
 //     let indexesMap = maps.get(mapId);
 //     console.log(indexesMap);
