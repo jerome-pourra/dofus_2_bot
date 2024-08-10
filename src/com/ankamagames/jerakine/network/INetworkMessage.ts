e85fc8fe1cfb7e25b12c04db65a0f17b34af09e5
Jérôme Pourra <j.pourra.perso@gmail.com>
2024-08-10 15:12:38 +0200

    add increase / decrease into interface

--- src/com/ankamagames/jerakine/network/INetworkMessage.ts
+++ src/com/ankamagames/jerakine/network/INetworkMessage.ts
@@ -4,7 +4,11 @@ import { ICustomDataOutput } from "./ICustomDataOutput";
 export interface INetworkMessage {
 
     getMessageId(): number;
+    
     getInstanceId(): number;
+    increaseInstanceId(): void;
+    decreaseInstanceId(): void;
+    
     pack(param1: ICustomDataOutput): void;
     unpack(param1: ICustomDataInput, param2: number): void;
     
