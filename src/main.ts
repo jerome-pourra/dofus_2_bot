2960784dd9b8fdd8b727f9f360facefc799d8b44
Jérôme Pourra <j.pourra.perso@gmail.com>
2024-08-10 15:04:00 +0200

    toto

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { Mitm } from "./network/Mitm";
 import { config } from "./config";
 import { FridaHookManager } from "./frida/hook";
-import { PakProtocol2 } from './com/ankamagames/jerakine/resources/protocols/impl/PakProtocol2';
 
-let maps = PakProtocol2.getFilesIndex("C:/Users/jerom/AppData/Local/Ankama/Dofus/content/maps/maps0.d2p");
+// let maps = PakProtocol2.getFilesIndex("C:/Users/jerom/AppData/Local/Ankama/Dofus/content/maps/maps0.d2p");
 // console.log(maps);
 
 // import * as zlib from 'zlib';
